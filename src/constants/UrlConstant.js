6e29fd5a875a2cded131905540c5b0b0c07ecd82
NZTR\ektap <ekta.prajapati@nztr.co.nz>
2025-03-13 15:59:05 +1300

    Change endpoint url

--- src/constants/UrlConstant.js
+++ src/constants/UrlConstant.js
@@ -1,7 +1,6 @@
 const urls = {
     // BASE_URL: "http://10.0.2.2:5298/api", //For android emulator 
-    // BASE_URL: "http://192.168.20.67:5298/api", //For android device
-    BASE_URL: "http://192.168.20.41:8080/api", //For ios
+    BASE_URL: "http://192.168.20.41:8080/api", //For android device
     
     MICROCHIP: {
       GetHorseByMicrochipNumber: (microchipNumber) => `${urls.BASE_URL}/Microchip/${microchipNumber}`,
