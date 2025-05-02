80257f3e0daf5f1b0123f8dfb7d23576a27a9b61
NZTR\ektap <ekta.prajapati@nztr.co.nz>
2025-05-02 13:14:48 +1200

    Modify function that convert UTC datetime to local.

--- src/screens/HorseDetail.jsx
+++ src/screens/HorseDetail.jsx
@@ -24,12 +24,12 @@ const HorseDetail = ({ route }) => {
   const [horseMessage, setHorseMessage] = useState("");
 
   function convertUTCDateToLocalDate(date) {
-    var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
+    var newDate = new Date(date.getTime() - date.getTimezoneOffset()*60*1000);
 
-    var offset = date.getTimezoneOffset() / 60;
-    var hours = date.getHours();
+    // var offset = date.getTimezoneOffset() / 60;
+    // var hours = date.getHours();
 
-    newDate.setHours(hours - offset);
+    // newDate.setHours(hours - offset);
 
     return newDate;   
 }
