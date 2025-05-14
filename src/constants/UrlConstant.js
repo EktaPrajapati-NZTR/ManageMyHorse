f10b7fb987a1e87e9c0fca2f3901c816342e7388
NZTR\ektap <ekta.prajapati@nztr.co.nz>
2025-05-14 16:15:42 +1200

    Modify url config.

--- src/constants/UrlConstant.js
+++ src/constants/UrlConstant.js
@@ -2,13 +2,14 @@ const urls = {
     // BASE_URL: "http://10.0.2.2:5298/api/", //Android emulator 
     // BASE_URL: "http://192.168.20.52:8080/api/", // Locally hosted
     // BASE_URL: "http://192.168.20.52:5298/api/", // Local
-    // BASE_URL: "https://lovetracingstage.nzracing.co.nz/api/", //Staging
-    BASE_URL: "https://idmytbapi.nzracing.co.nz/api/", //Production,
+    BASE_URL: "https://lovetracingstage.nzracing.co.nz/api/", //Staging
+    // BASE_URL: "https://idmytbapi.nzracing.co.nz/api/", //Production,
     LOGIN_URL: "https://appservice.nzracing.co.nz/1.2.0/",
     
     MICROCHIP: {
-      GetHorseByMicrochipNumber: (microchipNumber) => `${urls.BASE_URL}Microchip/${microchipNumber}`,
+      GetHorseByMicrochipNumber: (microchipNumber) => `${urls.BASE_URL}Microchip/by-microchip/${microchipNumber}`,
       SaveHorseLocation: () => `${urls.BASE_URL}Microchip/SaveHorseLocation`,
+      GetScannedHorsesByContactID: (contactID) => `${urls.BASE_URL}Microchip/by-contact/${contactID}`,
     },
 
     LOGIN:{
