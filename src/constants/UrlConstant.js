707b18d9efcc17f1502ca18c74fe9476b07311ba
NZTR\ektap <ekta.prajapati@nztr.co.nz>
2025-05-19 10:02:42 +1200

    Comment out unused API URL in URL constants.

--- src/constants/UrlConstant.js
+++ src/constants/UrlConstant.js
@@ -2,8 +2,8 @@ const urls = {
     // BASE_URL: "http://10.0.2.2:5298/api/", //Android emulator 
     // BASE_URL: "http://192.168.20.52:8080/api/", // Locally hosted
     // BASE_URL: "http://192.168.20.52:5298/api/", // Local
-    BASE_URL: "https://lovetracingstage.nzracing.co.nz/api/", //Staging
-    // BASE_URL: "https://idmytbapi.nzracing.co.nz/api/", //Production,
+    // BASE_URL: "https://lovetracingstage.nzracing.co.nz/api/", //Staging
+    BASE_URL: "https://idmytbapi.nzracing.co.nz/api/", //Production,
     LOGIN_URL: "https://appservice.nzracing.co.nz/1.2.0/",
     
     MICROCHIP: {
@@ -15,7 +15,7 @@ const urls = {
     LOGIN:{
       Login: () => `${urls.LOGIN_URL}Settings/Login`,
       Logout: () => `${urls.LOGIN_URL}Settings/Logout`,
-      VerifyAuthentication: () => `${urls.LOGIN_URL}Settings/VerifyAuthentication`,
+      // VerifyAuthentication: () => `${urls.LOGIN_URL}Settings/VerifyAuthentication`,
     }
 
   };
